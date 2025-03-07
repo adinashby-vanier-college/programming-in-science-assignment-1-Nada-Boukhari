5f84c6e048dfa8110180b8b573ca776a7bc27a39
Nada-Boukhari <nada.bkr17@gmail.com>
2025-03-07 12:50:53 +0000

    q1-3 done

--- Assignment1.py
+++ Assignment1.py
@@ -1,20 +1,24 @@
 # Function 1: Using Python built-in functions
 # This function should take three numbers as input and return their max.
 def built_in_functions_max(num1, num2, num3):
-    # TODO: Implement this function
-    pass  # Replace with your code
+    numbers = num1, num2, num3
+    return max(numbers)
 
 # Function 2: Using Python built-in functions
 # This function should take three numbers as input and return their min.
 def built_in_functions_min(num1, num2, num3):
-    # TODO: Implement this function
-    pass  # Replace with your code
+    numbers = num1, num2, num3
+    return min(numbers)
 
 # Function 3: Conditional Statements – The If Statement
 # This function should check if a number is positive, negative, or zero and return the corresponding message.
 def check_number(number):
-    # TODO: Implement this function
-    pass  # Replace with your code
+    if number < 0:
+        return "Negative"
+    if number > 0:
+        return "Positive"
+    if number == 0:
+        return "Zero"
 
 # Function 4: For Loop – Making a Star Shape
 # This function should return a string representing a star shape.
